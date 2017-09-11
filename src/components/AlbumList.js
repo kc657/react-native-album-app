9c14f153cf5d3b6026a0f31b33e9069e5eecc1c2
kc657 <kvn.0218@gmail.com>
2017-09-11 01:20:18 -0700

    passed albumtitle into albumdetail component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {View, Text} from 'react-native'
+import AlbumDetail from './AlbumDetail'
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text> Album List! </Text>
-    </View>
-  )
+class AlbumList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      albums: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => response.json())
+    .then(responseData => this.setState({albums:responseData}))
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    )
+  }
+
+  render() {
+    console.log(this.state.albums);
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    )
+  }
 }
 
 export default AlbumList

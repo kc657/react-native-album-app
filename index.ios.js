d9e0b977e091eb73ce3cedb94827bb7692611193
kc657 <kvn.0218@gmail.com>
2017-09-10 23:45:45 -0700

    Created header component and nested into index

---
+++ index.ios.js
@@ -0,0 +1,12 @@
+// import a library to help create a Component
+import React from 'react'
+import {AppRegistry} from 'react-native'
+import Header from './src/components/header'
+
+// create a Component
+const App = () => (
+  <Header />
+)
+
+// render it to the device
+AppRegistry.registerComponent('albums', () => App)
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// import libraries
+import React from 'react'
+import {Text} from 'react-native'
+
+// make Component
+const Header = () => {
+  return (
+    <Text> Albums </Text>
+  )
+}
+
+// make the component available to other parts
+export default Header

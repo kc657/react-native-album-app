aa6e06c844143d93b70ef793ca1e47b237eb44f3
kc657 <kvn.0218@gmail.com>
2017-10-02 17:04:26 -0700

    Added image to albumdetail and refactored out props.album in line 6-8

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react'
-import {Text} from 'react-native'
+import {Text , View , Image} from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image} = album
+  const {thumbnailStyle, headerContentStyle} = styles
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image
+            source={{uri:thumbnail_image}}
+            style={thumbnailStyle}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 }
 
 export default AlbumDetail

4d2708eaf28e64feca8f0cec06c6cfe0f04566b4
kc657 <kvn.0218@gmail.com>
2017-10-02 17:19:53 -0700

    Added album artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,35 +4,55 @@ import Card from './Card'
 import CardSection from './CardSection'
 
 const AlbumDetail = ({album}) => {
-  const {title, artist, thumbnail_image} = album
-  const {thumbnailStyle, headerContentStyle} = styles
+  const {title, artist, thumbnail_image, image} = album
+  const {thumbnailStyle, headerContainerStyle, thumbnailContainerStyle, headerTextStyle, albumImageStyle} = styles
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             source={{uri:thumbnail_image}}
             style={thumbnailStyle}
           />
         </View>
-        <View style={headerContentStyle}>
-          <Text>{title}</Text>
+        <View style={headerContainerStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{uri:image}}
+          style={albumImageStyle}
+        />
       </CardSection>
     </Card>
   )
 }
 
 const styles = {
-  headerContentStyle: {
-    flexDirection: 'column',
-    justifyContent: 'space-around'
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  headerTextStyle: {
+    fontSize: 18
   },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  headerContainerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  albumImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 

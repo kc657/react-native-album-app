33b32aedced62757b131c08cf70af593c96cb072
kc657 <kvn.0218@gmail.com>
2017-10-02 17:22:22 -0700

    Changed to scrollview

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {ScrollView} from 'react-native'
 import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
@@ -25,9 +25,9 @@ class AlbumList extends Component {
   render() {
     console.log(this.state.albums);
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     )
   }
 }
